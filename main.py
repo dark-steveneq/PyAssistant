207802173ac507c2de10d285303419993c0709bb
dark-steveneq <83212176+dark-steveneq@users.noreply.github.com>
2022-02-23 21:11:02 +0000

    Made functions work

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from lib import *
-from os import getcwd,chdir
-from importlib import import_module
+from os import listdir,chdir
+from importlib.machinery import SourceFileLoader
 
 tts = tts()
 #inp = detection().run()
-inp = "what's the time"
+inp = "test"
 print(inp)
 
-chdir("./func")
-functions = []
-functions = getcwd()
+chdir(r"func")
+
+files = listdir()
+funcfiles = [s.removeprefix("__pycache__") for s in files]
+functions = [s.removesuffix(".py") for s in funcfiles]
+current = 0
+
 for i in functions:
-    function = import_module(i)
-    output = function.func(inp, tts)
+    function = SourceFileLoader(i,funcfiles[current]).load_module()
+    output = False
+    for i in function.activators:
+        if inp == i:
+            output = function.func(inp, tts)
     if output == True:
-        pass
+        pass        
+    current += 1
 if output == False:
     tts.say("Sorry, i don't have that function")
